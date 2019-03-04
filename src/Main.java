6ed0b92f9837c0b968e669eb001809e7c00ca4aa
Rob <rsaund05@uoguelph.ca>
2019-03-04 16:45:51 -0500

    Removed leftover clutter from 'tiny_flex' days

--- src/Main.java
+++ src/Main.java
@@ -18,16 +18,26 @@ class Main {
   public static boolean SHOW_TREE = false;
   static public void main(String argv[]) {    
     /* Start the parser */
+
+    //Checking for valid file given in command line
+    File test = new File(argv[0]);
+    if(!(test.exists() && !test.isDirectory())){
+      System.out.println("Error: Invalid file given. Exiting.");
+      System.exit(0);
+    }
+
+    //Check for '-a' command, to know whether to print the AST 
     if (argv.length >= 2){
       if(argv[1].equals("-a")) SHOW_TREE = true;
     } else{
       SHOW_TREE = false;
     }
 
+    //Begin parsing
     try {
      parser p = new parser(new Lexer(new FileReader(argv[0])));
      Absyn result = (Absyn)(p.parse().value);      
-       if (SHOW_TREE) {
+       if (SHOW_TREE) { //Print out Abstract Syntax Tree
          System.out.println("The abstract syntax tree is:");
          ShowTreeVisitor visitor = new ShowTreeVisitor();
          result.accept(visitor, 0); 
