8b62c96532da2856836a7261c38a3c11bdf3d4af
Curtis Collins <curtis_collins@hotmail.ca>
2019-03-14 14:40:39 -0400

    Update SymbolTableVisitor.java

--- src/c1Absyn/SymbolTableVisitor.java
+++ src/c1Absyn/SymbolTableVisitor.java
@@ -3,7 +3,8 @@ import c1Absyn.*;
 public class SymbolTableVisitor implements AbsynVisitor {
 
   final static int SPACES = 4;
-  HashMap <Defined> symTable = new HashMap<>();
+  Arraylist<Defined> definitions = nwe ArrayList<Defined>();
+  HashMap <String> symTable = new HashMap<>();
 
   private void indent( int level ) {
     for( int i = 0; i < level * SPACES; i++ ) System.out.print( " " );
@@ -40,17 +41,11 @@ public class SymbolTableVisitor implements AbsynVisitor {
   }
 
   public void visit( AssignExp exp, int level ) {
-    indent( level );
-    System.out.println( "AssignExp:" );
-    level++;
     exp.lhs.accept( this, level );
     exp.rhs.accept( this, level );
   }
 
   public void visit( IfExp exp, int level ) {
-    indent( level );
-    System.out.println( "IfExp:" );
-    level++;
     exp.test.accept( this, level );
     exp.thenpart.accept( this, level );
     if (exp.elsepart != null )
@@ -63,51 +58,35 @@ public class SymbolTableVisitor implements AbsynVisitor {
   }
 
   public void visit( OpExp exp, int level ) {
-    indent( level );
-    System.out.print( "OpExp:" ); 
     switch( exp.op ) {
       case OpExp.PLUS:
-        System.out.println( " + " );
         break;
       case OpExp.MINUS:
-        System.out.println( " - " );
         break;
       case OpExp.MUL:
-        System.out.println( " * " );
         break;
       case OpExp.DIV:
-        System.out.println( " / " );
         break;
       case OpExp.EQ:
-        System.out.println( " == " );
         break;
       case OpExp.NE:
-        System.out.println(" != ");
         break;
       case OpExp.LT:
-        System.out.println( " < " );
         break;
       case OpExp.LE:
-        System.out.println(" <= ");
         break;
       case OpExp.GT:
-        System.out.println( " > " );
         break;
       case OpExp.GE:
-        System.out.println(" >= ");
         break;
       default:
-        System.out.println( "Unrecognized operator at line " + exp.row + " and column " + exp.col);
+        break;
     }
-    level++;
     exp.left.accept( this, level );
     exp.right.accept( this, level );
   }
 
   public void visit( VarExp exp, int level ) {
-    indent( level );
-    System.out.println( "VarExp: ");
-    level++;
 
     if (exp.variable != null)
       exp.variable.accept(this, level);
@@ -116,7 +95,6 @@ public class SymbolTableVisitor implements AbsynVisitor {
 //still need to finish
 //ArrayDec
 public void visit(ArrayDec exp, int level ) {
-  indent( level );
   String ty = new String("");
 
   if (exp.typ.typ == NameTy.VOID)
@@ -127,29 +105,20 @@ public void visit(ArrayDec exp, int level ) {
   if (exp.size != null)
   {
     if (!exp.size.value.equals(null))
-      System.out.println("ArrayDec: " + exp.name + "[" + exp.size.value + "]" + " - " + ty);
   }
   else
-    System.out.println("ArrayDec: " + exp.name + "[]" + " - " + ty);
 
 }
 
 //CallExp
 public void visit(CallExp exp, int level ) {
-  indent( level );
-  System.out.println("CallExp: " + exp.func);
-  level++;
+
   if (exp.args != null)
     exp.args.accept(this, level);
 }
 
 //CompoundExp
 public void visit(CompoundExp exp, int level ) {
-  indent( level );
-  System.out.println("CompoundExp: ");
-  
-  if (exp.decs != null && exp.exp != null)
-    level++;
 
   if (exp.decs != null)
     exp.decs.accept(this, level);
@@ -159,13 +128,9 @@ public void visit(CompoundExp exp, int level ) {
 
 //FunctionDec
 public void visit(FunctionDec exp, int level ) {
-  indent( level );
   if (exp.result.typ == NameTy.VOID)
-    System.out.println("FunctionDec: " + exp.func + " - VOID");
   else if (exp.result.typ == NameTy.INT)
-  System.out.println("FunctionDec: " + exp.func + " - INT"); 
 
-  level++;
 
   if (exp.params != null)
     exp.params.accept(this, level);
@@ -176,48 +141,31 @@ public void visit(FunctionDec exp, int level ) {
 
 //IndexVar
 public void visit(IndexVar exp, int level ) {
-  indent( level );
-  System.out.println("IndexVar: " + exp.name);
-  level++;
   exp.index.accept(this, level);
 }
 
 //NilExp
 public void visit(NilExp exp, int level ) {
-  indent( level );
-  System.out.println("NilExp:");
 }
 
 //ReturnExp
 public void visit(ReturnExp exp, int level ) {
-  indent( level );
-  System.out.println("ReturnExp: ");
-  level++;
-
   if (exp.exp != null)
     exp.exp.accept(this, level);
 }
 
 //SimpleDec
 public void visit(SimpleDec exp, int level ) {
-  indent( level );
   if (exp.typ.typ == NameTy.VOID)
-    System.out.println("SimpleDec: " + exp.name + " - VOID"); 
   else if (exp.typ.typ == NameTy.INT)
-    System.out.println("SimpleDec: " + exp.name + " - INT");
 }
 
 //SimpleVar
 public void visit(SimpleVar exp, int level ) {
-  indent( level );
-  System.out.println("SimpleVar: " + exp.name);
 }
 
 //WhileExp
 public void visit(WhileExp exp, int level ) {
-  indent( level );
-  System.out.println("WhileExp: ");
-  level++;
   if (exp.test != null)
     exp.test.accept(this, level);
   if (exp.body != null)
