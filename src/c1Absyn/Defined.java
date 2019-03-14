af39f1ec2b290e028b4c474715af122b1c5cdb60
Curtis Collins <curtis_collins@hotmail.ca>
2019-03-14 13:14:54 -0400

    Added some new files for checkpoint 2

---
+++ src/c1Absyn/Defined.java
@@ -0,0 +1,13 @@
+import c1Absyn.*;
+
+public class Defined 
+{
+    public String name;
+    public String type;  
+
+    public Defined (String name, String type)
+    {
+        this.name = name;
+        this.type = type;
+    }
+}
