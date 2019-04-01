5061335637dd2d22dd724590188d1d000eb2a9e4
Curtis Collins <curtis_collins@hotmail.ca>
2019-04-01 18:27:49 -0400

    made things work

--- src/CodeGen.java
+++ src/CodeGen.java
@@ -16,7 +16,7 @@ public class CodeGen {
 	public static int ac = 0;
 	public static int ac1 = 1;
 
-	public static int line = 0; //Variable to track line number in output file, starts at line after prelude
+	//public static int line = 0; //Variable to track line number in output file, starts at line after prelude
 	public static int entry = 0;
 	public static int globalOffset = 0;
 
@@ -24,21 +24,58 @@ public class CodeGen {
 	public static int retFO = -1;
 	public static int initFO = -2;
 
-
+	public static int emitLoc = 0;
+	public static int highEmitLoc = 0;
 
 	public static void emitRM(String opCode, int r1, int offset, int r2, String comment) {
-		System.out.println(line + ": " + opCode + " " + r1 + ", " + offset + "(" + r2 + ") \t" + comment);
-		line++;
+		System.out.println(emitLoc + ": " + opCode + " " + r1 + ", " + offset + "(" + r2 + ") \t" + comment);
+		emitLoc++;
+
+		if (highEmitLoc < emitLoc)
+			highEmitLoc = emitLoc;
 	}
 
 	public static void emitRO(String opCode, int r1, int r2, int r3, String comment){
-		System.out.println(line + ": " + opCode + " " + r1 + ", " + r2 + ", " + r3 + " \t" + comment);
-		line++;
+		System.out.println(emitLoc + ": " + opCode + " " + r1 + ", " + r2 + ", " + r3 + " \t" + comment);
+		emitLoc++;
+
+		if (highEmitLoc < emitLoc)
+			highEmitLoc = emitLoc;
 	}
 
 	public static void emitRM_Abs(String opCode, int r, int a, String comment){
-		System.out.println(line + ": " + opCode + " " + r + " " + (a - (line + 1)) + "(" + pc + ") \t" + comment);
-		line++;
+		System.out.println(emitLoc + ": " + opCode + " " + r + " " + (a - (emitLoc + 1)) + "(" + pc + ") \t" + comment);
+		emitLoc++;
+
+		if (highEmitLoc < emitLoc)
+			highEmitLoc = emitLoc;
+	}
+
+	public static void emitComment(String comment)
+	{
+		System.out.println("* " + comment);
+	}
+
+	public static int emitSkip(int distance)
+	{
+		int i = emitLoc;
+		if (highEmitLoc < emitLoc)
+			highEmitLoc = emitLoc;
+
+		emitLoc += distance;
+		return i;
+	}
+
+	public static void emitRestore()
+	{
+		emitLoc = highEmitLoc;
+	}
+
+	public static void emitBackup(int loc)
+	{
+		if (loc > highEmitLoc)
+			emitComment("BUG in emitBackup");
+		emitLoc = loc;
 	}
 
 	public static void codeGen() throws Exception{
@@ -50,9 +87,25 @@ public class CodeGen {
 		
 
 		//Printing prelude
+		emitComment("C-Minus Compilation to TM Code");
+		emitComment("File: filename.tm");
+		emitComment("Standard prelude:");
 		emitRM("LD", 6, 0, 0, "load gp with maxaddr");
 		emitRM("LDA", 5, 0, 6, "Copy gp to fp");
 		emitRM("ST", 0, 0, 0, "Clear content at loc");
+		int savedLoc = emitSkip(1);
+		emitComment("Jump around i/o routines here");
+		emitComment("code for input routine");
+		emitRM("ST", 0, -1, 5, "store return");
+		emitRO("IN", 0, 0, 0, "input");
+		emitRM("LD", 7, -1, 5, "return to caller");
+		emitComment("code for output routine");
+		emitRM("ST", 0,-1,5, "load output value");
+		emitRO("OUT", 0, 0, 0, "output");
+		emitRM("LD", 7, -1, 5, "return to caller");
+		emitBackup(savedLoc);
+		emitRM("LDA", 7, 7, 7, "jump around i/o code");
+		emitRestore();
 
 		//Rest of the stuff lol
 
