f11c69de318c8a12a0f9baf2453bb1f3b17e8c27
Rob Saunders <rsaund05@uoguelph.ca>
2019-04-02 13:40:26 -0400

    Update CodeGen.java

--- src/CodeGen.java
+++ src/CodeGen.java
@@ -121,51 +121,51 @@ public class CodeGen implements AbsynVisitor {
 	}
 
 	  public void visit( ExpList expList, int level ) {
-    // while( expList != null ) {
-    //   if (expList.head != null)
-    //     expList.head.accept( this, level );
-    //   expList = expList.tail;
-    // } 
+    while( expList != null ) {
+      if (expList.head != null)
+        expList.head.accept( this, level );
+      expList = expList.tail;
+    } 
   }
 
   public void visit (VarDecList varDecList, int level)
   {
-    // while(varDecList != null)
-    // {
-    //   if (varDecList.head != null)
-    //     varDecList.head.accept(this, level);
+    while(varDecList != null)
+    {
+      if (varDecList.head != null)
+        varDecList.head.accept(this, level);
 
-    //   varDecList = varDecList.tail;
-    // }
+      varDecList = varDecList.tail;
+    }
   }
 
   public void visit (DecList decList, int level)
   {
-    // while(decList != null)
-    // {
-    //   if (decList.head != null)
-    //     decList.head.accept(this, level);
+    while(decList != null)
+    {
+      if (decList.head != null)
+        decList.head.accept(this, level);
 
-    //   decList = decList.tail;
-    // }
+      decList = decList.tail;
+    }
   }
 
   public void visit( AssignExp exp, int level ) {
  
     // System.out.println( "AssignExp:" );
-    // level++;
-    // exp.lhs.accept( this, level );
-    // exp.rhs.accept( this, level );
+    level++;
+    exp.lhs.accept( this, level );
+    exp.rhs.accept( this, level );
   }
 
   public void visit( IfExp exp, int level ) {
  
     //System.out.println( "IfExp:" );
     level++;
-    // exp.test.accept( this, level );
-    // exp.thenpart.accept( this, level );
-    // if (exp.elsepart != null )
-    //    exp.elsepart.accept( this, level );
+    exp.test.accept( this, level );
+    exp.thenpart.accept( this, level );
+    if (exp.elsepart != null )
+       exp.elsepart.accept( this, level );
   }
 
   public void visit( IntExp exp, int level ) {
@@ -211,8 +211,8 @@ public class CodeGen implements AbsynVisitor {
         //System.out.println( "Unrecognized operator at line " + exp.row + " and column " + exp.col);
     }
     level++;
-    // exp.left.accept( this, level );
-    // exp.right.accept( this, level );
+    exp.left.accept( this, level );
+    exp.right.accept( this, level );
   }
 
   public void visit( VarExp exp, int level ) {
@@ -220,26 +220,26 @@ public class CodeGen implements AbsynVisitor {
     //System.out.println( "VarExp: ");
     level++;
 
-    // if (exp.variable != null)
-    //   exp.variable.accept(this, level);
+    if (exp.variable != null)
+      exp.variable.accept(this, level);
   }
 
 //still need to finish
 //ArrayDec
 public void visit(ArrayDec exp, int level ) {
-  String ty = new String("");
+  // String ty = new String("");
 
-  //if (exp.typ.typ == NameTy.VOID)
+  // if (exp.typ.typ == NameTy.VOID)
   //    ty = new String("VOID");
-  //else if (exp.typ.typ == NameTy.INT)
+  // else if (exp.typ.typ == NameTy.INT)
   //    ty = new String("INT");
 
-  //if (exp.size != null)
-  //{
+  // if (exp.size != null)
+  // {
   //  if (!exp.size.value.equals(null))
-      //System.out.println("ArrayDec: " + exp.name + "[" + exp.size.value + "]" + " - " + ty);
-  //}
-  //else
+  //     //System.out.println("ArrayDec: " + exp.name + "[" + exp.size.value + "]" + " - " + ty);
+  // }
+  // else
     //System.out.println("ArrayDec: " + exp.name + "[]" + " - " + ty);
 
 }
@@ -248,44 +248,44 @@ public void visit(ArrayDec exp, int level ) {
 public void visit(CallExp exp, int level ) {
   //System.out.println("CallExp: " + exp.func);
   level++;
-  //if (exp.args != null)
-    //exp.args.accept(this, level);
+  if (exp.args != null)
+    exp.args.accept(this, level);
 }
 
 //CompoundExp
 public void visit(CompoundExp exp, int level ) {
   //System.out.println("CompoundExp: ");
   
-  //if (exp.decs != null && exp.exp != null)
+  if (exp.decs != null && exp.exp != null)
     level++;
 
-  //if (exp.decs != null)
-    //exp.decs.accept(this, level);
-  //if (exp.exp != null)
-    //exp.exp.accept(this, level);
+  if (exp.decs != null)
+    exp.decs.accept(this, level);
+  if (exp.exp != null)
+    exp.exp.accept(this, level);
 }
 
 //FunctionDec
 public void visit(FunctionDec exp, int level ) {
   //if (exp.result.typ == NameTy.VOID)
     //System.out.println("FunctionDec: " + exp.func + " - VOID");
-  //else if (exp.result.typ == NameTy.INT)
+ // else if (exp.result.typ == NameTy.INT)
   //System.out.println("FunctionDec: " + exp.func + " - INT"); 
 
   level++;
 
-  //if (exp.params != null)
-    //exp.params.accept(this, level);
+  if (exp.params != null)
+    exp.params.accept(this, level);
 
-  //if (exp.body != null)
-    //exp.body.accept(this, level);
+  if (exp.body != null)
+    exp.body.accept(this, level);
 }
 
 //IndexVar
 public void visit(IndexVar exp, int level ) {
   //System.out.println("IndexVar: " + exp.name);
   level++;
-  //exp.index.accept(this, level);
+  exp.index.accept(this, level);
 }
 
 //NilExp
@@ -298,8 +298,8 @@ public void visit(ReturnExp exp, int level ) {
   //System.out.println("ReturnExp: ");
   level++;
 
-  //if (exp.exp != null)
-    //exp.exp.accept(this, level);
+  if (exp.exp != null)
+    exp.exp.accept(this, level);
 }
 
 //SimpleDec
@@ -319,10 +319,10 @@ public void visit(SimpleVar exp, int level ) {
 public void visit(WhileExp exp, int level ) {
   //System.out.println("WhileExp: ");
   level++;
-  //if (exp.test != null)
-    //exp.test.accept(this, level);
-  //if (exp.body != null)
-    //exp.body.accept(this, level);
+  if (exp.test != null)
+    exp.test.accept(this, level);
+  if (exp.body != null)
+    exp.body.accept(this, level);
 }
 
 public void visit (NameTy exp, int level)
